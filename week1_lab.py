2ca0669110804359d397038245c62460906bac4b
Ujala19 <26627761@edgehill.ac.uk>
2025-10-07 15:49:14 +0100

    Add files via upload
    Week1_lab.py

---
+++ week1_lab.py
@@ -0,0 +1,23 @@
+print ("Hello, World!")
+
+# storing text (a string) 
+student_name = "Ujala"
+
+# storing a whole numbner (an integer) 
+student_age = 22
+
+#print the variables to the console
+print("student Name:", student_name)
+print("student_age", student_age)
+
+# A simple calculation
+
+num1 = 11
+num2 = 19
+total = num1 + num2 
+
+print("The sum of this is:", total)
+
+git add week1_lab.py
+git commit -m "Feat: Complete Week 1 lab exercises"
+git push
